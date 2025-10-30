ecc8a25a8fea1682319ecddd8eea93e084e5b21e
irgendwasmitfabi <fabian.conrad0304@gmail.com>
2025-10-30 20:58:40 +0100

    Make remove button red

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -27,9 +27,9 @@
                                 </button>
                             </div>
                             <div v-if="showDoneEntities" class="flex items-center">
-                                <button @click="deactivateEntity(entity.id)" type="button" class="hidden w-6 h-6 group-hover:block text-blue-700 border-blue-700 hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-blue-300 font-medium rounded-full text-center items-center">
-                                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                <button @click="deactivateEntity(entity.id)" type="button" class="hidden w-6 h-6 group-hover:block text-red-700 border-red-700 hover:bg-red-700 hover:text-white focus:outline-none focus:ring-blue-300 font-medium rounded-full text-center items-center">
+                                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd"/>
                                     </svg>
                                 </button>
                             </div>
